fc6f41865095123e1a8532a563abd5b78459ab8f
Adil Muhammad <ivgcomunity@gmail.com>
2025-06-11 16:07:36 +0700

    add footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,21 @@
+export default function Footer() {
+  return (
+    <footer className="mt-5 py-4 text-center text-white">
+      <p className="text-sm text-inherit">
+        &copy; {new Date().getFullYear()} Browser Extension Magazine. All rights
+        reserved.
+      </p>
+      <p className="mt-2 text-xs text-inherit">
+        Made with ❤️ by{" "}
+        <a
+          className="text-inherit"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.frontendmentor.io/profile/iniadil"
+        >
+          Muhammad Adil
+        </a>
+      </p>
+    </footer>
+  );
+}
